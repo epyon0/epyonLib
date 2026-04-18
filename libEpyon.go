f8fc943eabd3d427db46b39f9708be58c89a17aa
gibersonja <gibersonja@gmail.com>
2026-04-18 15:09:54 -0500

    add ANSI functions

--- libEpyon.go
+++ libEpyon.go
@@ -24,34 +24,34 @@ type LinkedList struct {
 	length int
 }
 
-// Data func to export struct field
+// Returns the data from that node of the linkedlist
 func (n *Node) Data() any {
 	return n.data
 }
 
-// Next func to export struct field
+// Returns the next node in the linkedlist
 func (n *Node) Next() *Node {
 	return n.next
 }
 
-// Head func to export struct field
+// Returns node of the head of the linkedlist
 func (l *LinkedList) Head() *Node {
 	return l.head
 }
 
-// Length func to export struct field
+// Returns length of linkedlist
 func (l *LinkedList) Length() int {
 	return l.length
 }
 
-// Push adds item to the begining of a linkedlist
+// Adds item to the begining of a linkedlist
 func (l *LinkedList) Push(data any) {
 	newNode := &Node{data: data, next: l.head}
 	l.head = newNode
 	l.length++
 }
 
-// Pop removes item from the begining of a linkedlist
+// Removes item from the begining of a linkedlist
 func (l *LinkedList) Pop() (any, error) {
 	var data any
 	var err error
@@ -67,12 +67,12 @@ func (l *LinkedList) Pop() (any, error) {
 	return data, err
 }
 
-// Peek returns the data at the head of a linkedlist without modification
+// Returns the data at the head of a linkedlist without modification
 func (l *LinkedList) Peek() any {
 	return l.Head().Data()
 }
 
-// IsEmpty returns if the linkedlist is empty or not
+// Returns if the linkedlist is empty or not
 func (l *LinkedList) IsEmpty() bool {
 	if l.Head() == nil {
 		return true
@@ -81,7 +81,7 @@ func (l *LinkedList) IsEmpty() bool {
 	}
 }
 
-// Print returns string to display the value of a given node
+// Returns string to display the value of a given node
 func (l *LinkedList) Print(node *Node) string {
 	if node != nil {
 		return PrintValue(node.data)
@@ -90,7 +90,7 @@ func (l *LinkedList) Print(node *Node) string {
 	return ""
 }
 
-// PrintAll returns a string to display the value of the entire linkedlist
+// Returns a string to display the value of the entire linkedlist
 func (l *LinkedList) PrintAll() string {
 	var output string
 	currentNode := l.Head()
@@ -129,12 +129,12 @@ func (l *LinkedList) PrintAll() string {
 	return output
 }
 
-// MakeLinkedList creates a new linkedlist
+// Creates a new linkedlist
 func MakeLinkedList() *LinkedList {
 	return &LinkedList{head: nil, length: 0}
 }
 
-// PipeRead returns byte slice of data from the pipe
+// Returns byte slice of data from the pipe
 func PipeRead(bufSize int) ([]byte, error) {
 	r := bufio.NewReader(os.Stdin)
 	buf := make([]byte, 0, bufSize)
@@ -164,7 +164,7 @@ func PipeRead(bufSize int) ([]byte, error) {
 	}
 }
 
-// PipeExists return a boolean based on if a named pipe into the program exists
+// Return a boolean based on if a named pipe into the program exists
 func PipeExists() (bool, error) {
 	fi, err := os.Stdin.Stat()
 
@@ -179,7 +179,7 @@ func PipeExists() (bool, error) {
 	}
 }
 
-// TruncString will truncate the given string to the given width
+// Truncate the given string to the given width
 func TruncString(text string, width int) string {
 	if width < 0 {
 		return ""
@@ -192,7 +192,7 @@ func TruncString(text string, width int) string {
 	return string(runes[:width])
 }
 
-// ClearScreen will clear the terminal on Windows or Linux
+// Clear the terminal on Windows or Linux
 func ClearScreen() error {
 	var cmd *exec.Cmd
 	if runtime.GOOS == "windows" {
@@ -210,260 +210,480 @@ type Ansi struct {
 	Line, Column, Count int
 }
 
-// CursorHome moves cursor to home position (0,0)
+// Moves cursor to home position (0,0)
 func (ansi Ansi) CurosrHome() {
 	fmt.Fprintf(os.Stdout, "\033[H")
 }
 
-// CursorMove moves cursor to line Ansi.Line, column Ansi.Column
+// Moves cursor to line Ansi.Line, column Ansi.Column
 func (ansi Ansi) CursorMove() {
 	fmt.Fprintf(os.Stdout, "\033[%d;%dH", ansi.Line, ansi.Column)
 }
 
-// CursorUp moves cursor up Ansi.Count lines
+// Moves cursor up Ansi.Count lines
 func (ansi Ansi) CursorUp() {
 	fmt.Fprintf(os.Stdout, "\033[%dA", ansi.Count)
 }
 
-// CursorDown moves cursor down Ansi.Count lines
+// Moves cursor down Ansi.Count lines
 func (ansi Ansi) CursorDown() {
 	fmt.Fprintf(os.Stdout, "\033[%dB", ansi.Count)
 }
 
-// CursorRight moves cursor right Ansi.Count lines
+// Moves cursor right Ansi.Count lines
 func (ansi Ansi) CursorRight() {
 	fmt.Fprintf(os.Stdout, "\033[%dC", ansi.Count)
 }
 
-// CursorLeft moves cursor left Ansi.Count lines
+// Moves cursor left Ansi.Count lines
 func (ansi Ansi) CursorLeft() {
 	fmt.Fprintf(os.Stdout, "\033[%dD", ansi.Count)
 }
 
-// CursorBeginningDown moves cursor to beginning of next line, Ansi.Count lines down
+// Moves cursor to beginning of next line, Ansi.Count lines down
 func (ansi Ansi) CursorBeginningDown() {
 	fmt.Fprintf(os.Stdout, "\033[%dE", ansi.Count)
 }
 
-// CursorBeginningUp moves cursor to beginning of previous line, Ansi.Count lines up
+// Moves cursor to beginning of previous line, Ansi.Count lines up
 func (ansi Ansi) CursorBeginningUp() {
 	fmt.Fprintf(os.Stdout, "\033[%dF", ansi.Count)
 }
 
-// CursorColumn moves cursor to column Ansi.Column
+// Moves cursor to column Ansi.Column
 func (ansi Ansi) CursorColumn() {
 	fmt.Fprintf(os.Stdout, "\033[%dG", ansi.Column)
 }
 
-// CursorReqPos request cursor position
+// Request cursor position
 func (ansi Ansi) CursorReqPos() {
 	fmt.Fprintf(os.Stdout, "\033[6n")
 }
 
-// CursorUpOne moves cursor one line up, scrolling if needed
+// Moves cursor one line up, scrolling if needed
 func (ansi Ansi) CursorUpOne() {
 	fmt.Fprintf(os.Stdout, "\033 M")
 }
 
-// CursorSavePos save cursor position (DEC) (recommended)
+// Save cursor position (DEC) (recommended)
 func (ansi Ansi) CursorSavePos() {
 	fmt.Fprintf(os.Stdout, "\033 7")
 }
 
-// CursorLoadPos restores the cursor the the last saved position (DEC)
+// Restores the cursor the the last saved position (DEC)
 func (ansi Ansi) CursorLoadPos() {
 	fmt.Fprintf(os.Stdout, "\033 8")
 }
 
-// CursorSavePosSCO save cursor position (SCO)
+// Save cursor position (SCO)
 func (ansi Ansi) CursorSavePosSCO() {
 	fmt.Fprintf(os.Stdout, "\033[s")
 }
 
-// CursorLoadPosSCO restores the cursor to the last saved position (SCO)
+// Restores the cursor to the last saved position (SCO)
 func (ansi Ansi) CursorLoadPosSCO() {
 	fmt.Fprintf(os.Stdout, "\033[u")
 }
 
-// CursorEraseScreenEnd erase from cursor until end of screen
+// Erase from cursor until end of screen
 func (ansi Ansi) CursorEraseScreenEnd() {
 	fmt.Fprintf(os.Stdout, "\033[0J")
 }
 
-// CursorEraseScreenBeginning erase from cursor to beginning of screen
+// Erase from cursor to beginning of screen
 func (ansi Ansi) CursorEraseScreenBeginning() {
 	fmt.Fprintf(os.Stdout, "\033[1J")
 }
 
-// CursorEraseScreenAll erase entire screen
+// Erase entire screen
 func (ansi Ansi) CursorEraseScreenAll() {
 	fmt.Fprintf(os.Stdout, "\033[2J")
 }
 
-// CursorEraseSavedLines erase saved lines
+// Erase saved lines
 func (ansi Ansi) CursorEraseSavedLines() {
 	fmt.Fprintf(os.Stdout, "\033[3J")
 }
 
-// CursorEraseLineEnd erase from cursor to end of line
+// Erase from cursor to end of line
 func (ansi Ansi) CursorEraseLineEnd() {
 	fmt.Fprintf(os.Stdout, "\033[0K")
 }
 
-// CursorEraseLineBeginning erase start of line to the cursor
+// Erase start of line to the cursor
 func (ansi Ansi) CursorEraseLineBeginning() {
 	fmt.Fprintf(os.Stdout, "\033[1K")
 }
 
-// CursorEraseLineAll erase the entire line
+// Erase the entire line
 func (ansi Ansi) CursorEraseLineAll() {
 	fmt.Fprintf(os.Stdout, "\033[2K")
 }
 
-// Reset reset all modes (styles and colors)
+// Make cursor invisible
+func (ansi Ansi) CursorInvisible() {
+	fmt.Fprintf(os.Stdout, "\033[?25l")
+}
+
+// Make cursor visible
+func (ansi Ansi) CursorVisible() {
+	fmt.Fprintf(os.Stdout, "\033[?25h")
+}
+
+// Restore screen
+func (ansi Ansi) ScreenRestore() {
+	fmt.Fprintf(os.Stdout, "\033[?47l")
+}
+
+// Save screen
+func (ansi Ansi) ScreenSave() {
+	fmt.Fprintf(os.Stdout, "\033[?47h")
+}
+
+// Enables the alternative buffer
+func (ansi Ansi) EnableAltBuffer() {
+	fmt.Fprintf(os.Stdout, "\033[?1049h")
+}
+
+// Disable the alternative buffer
+func (ansi Ansi) DisableAltBuffer() {
+	fmt.Fprintf(os.Stdout, "\033[?1049l")
+}
+
+// Reset all modes (styles and colors)
 func (ansi Ansi) Reset() {
 	fmt.Fprintf(os.Stdout, "\033[0m")
 }
 
-// TextBold set bold mode
+// Set bold mode
 func (ansi Ansi) TextBold() {
 	fmt.Fprintf(os.Stdout, "\033[1m")
 }
 
-// TextBoldReset reset bold mode (and dim/faint mode)
+// Reset bold mode (and dim/faint mode)
 func (ansi Ansi) TextBoldReset() {
 	fmt.Fprintf(os.Stdout, "\033[22m")
 }
 
-// TextDim set dim/faint mode
+// Set dim/faint mode
 func (ansi Ansi) TextDim() {
 	fmt.Fprintf(os.Stdout, "\033[2m")
 }
 
-// TextDimReset reset dim/faint mode (and bold mode)
+// Reset dim/faint mode (and bold mode)
 func (ansi Ansi) TextDimReset() {
 	ansi.TextBoldReset()
 }
 
-// TextFaint set dim/faint mode
+// Set dim/faint mode
 func (ansi Ansi) TextFaint() {
 	ansi.TextDim()
 }
 
-// TextFaintReset reset dim/faint mode (and bold mode)
+// Reset dim/faint mode (and bold mode)
 func (ansi Ansi) TextFaintReset() {
 	ansi.TextDimReset()
 }
 
-// TextItalic set italic mode
+// Set italic mode
 func (ansi Ansi) TextItalic() {
 	fmt.Fprintf(os.Stdout, "\033[3m")
 }
 
-// TextItalicReset reset italic mode
+// Reset italic mode
 func (ansi Ansi) TextItalicReset() {
 	fmt.Fprintf(os.Stdout, "\033[23m")
 }
 
-// TextUnderline set underline mode
+// Set underline mode
 func (ansi Ansi) TextUnderline() {
 	fmt.Fprintf(os.Stdout, "\033[4m")
 }
 
-// TextUnderlineReset reset underline mode
+// Reset underline mode
 func (ansi Ansi) TextUnderlineReset() {
 	fmt.Fprintf(os.Stdout, "\033[24m")
 }
 
-// TextBlinking set blinking mode
+// Set blinking mode
 func (ansi Ansi) TextBlinking() {
 	fmt.Fprintf(os.Stdout, "\033[5m")
 }
 
-// TextBlinkingReset reset blinking mode
+// Reset blinking mode
 func (ansi Ansi) TextBlinkingReset() {
 	fmt.Fprintf(os.Stdout, "\033[25m")
 }
 
-// TextInverse set inverse/reverse mode
+// Set inverse/reverse mode
 func (ansi Ansi) TextInverse() {
 	fmt.Fprintf(os.Stdout, "\033[7m")
 }
 
-// TextInverseReset reset inverse/reverse mode
+// Reset inverse/reverse mode
 func (ansi Ansi) TextInverseReset() {
 	fmt.Fprintf(os.Stdout, "\033[27m")
 }
 
-// TextReverse set inverse/reverse mode
+// Set inverse/reverse mode
 func (ansi Ansi) TextReverse() {
 	ansi.TextInverse()
 }
 
-// TextReverseReset reset inverse/reverse mode
+// Reset inverse/reverse mode
 func (ansi Ansi) TextReverseReset() {
 	ansi.TextInverseReset()
 }
 
-// TextHidden set hidden/invisible mode
+// Set hidden/invisible mode
 func (ansi Ansi) TextHidden() {
 	fmt.Fprintf(os.Stdout, "\033[8m")
 }
 
-// TextHiddenReset reset hidden/invisible mode
+// Reset hidden/invisible mode
 func (ansi Ansi) TextHiddenReset() {
 	fmt.Fprintf(os.Stdout, "\033[28m")
 }
 
-// TextInvisible set hidden/invisible mode
+// Set hidden/invisible mode
 func (ansi Ansi) TextInvisible() {
 	ansi.TextHidden()
 }
 
-// TextInvisibleReset reset hidden/invisible mode
+// Reset hidden/invisible mode
 func (ansi Ansi) TextInvisibleReset() {
 	ansi.TextHiddenReset()
 }
 
-// TextStrikethrough set strikethrough mode
+// Set strikethrough mode
 func (ansi Ansi) TextStrikethrough() {
 	fmt.Fprintf(os.Stdout, "\033[9m")
 }
 
-// TextStrikthroughReset reset strikethrough mode
+// Reset strikethrough mode
 func (ansi Ansi) TextStrikethroughReset() {
 	fmt.Fprintf(os.Stdout, "\033[29m")
 }
 
-/*
-	Color.Black.Foreground = 30
-	Color.Red.Foreground = 31
-	Color.Green.Foreground = 32
-	Color.Yellow.Foreground = 33
-	Color.Blue.Foreground = 34
-	Color.Magenta.Foreground = 35
-	Color.Cyan.Foreground = 36
-	Color.White.Foreground = 37
+// Set foreground color
+func (ansi Ansi) ColorBlackFG() {
+	fmt.Fprintf(os.Stdout, "\033[30m")
+}
 
-	Color.Black.Background = 40
-	Color.Red.Background = 41
-	Color.Green.Background = 42
-	Color.Yellow.Background = 43
-	Color.Blue.Background = 44
-	Color.Magenta.Background = 45
-	Color.Cyan.Background = 46
-	Color.White.Background = 47
-*/
+// Set background color
+func (ansi Ansi) ColorBlackBG() {
+	fmt.Fprintf(os.Stdout, "\033[40m")
+}
 
+// Set foreground color
+func (ansi Ansi) ColorRedFG() {
+	fmt.Fprintf(os.Stdout, "\033[31m")
+}
+
+// Set background color
+func (ansi Ansi) ColorRedBG() {
+	fmt.Fprintf(os.Stdout, "\033[41m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorGreenFG() {
+	fmt.Fprintf(os.Stdout, "\033[32m")
+}
+
+// Set background color
+func (ansi Ansi) ColorGreenBG() {
+	fmt.Fprintf(os.Stdout, "\033[42m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorYellowFG() {
+	fmt.Fprintf(os.Stdout, "\033[33m")
+}
+
+// Set background color
+func (ansi Ansi) ColorYellowBG() {
+	fmt.Fprintf(os.Stdout, "\033[43m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorBlueFG() {
+	fmt.Fprintf(os.Stdout, "\033[34m")
+}
+
+// Set background color
+func (ansi Ansi) ColorBlueBG() {
+	fmt.Fprintf(os.Stdout, "\033[44m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorMagentaFG() {
+	fmt.Fprintf(os.Stdout, "\033[35m")
+}
+
+// Set background color
+func (ansi Ansi) ColorMagentaBG() {
+	fmt.Fprintf(os.Stdout, "\033[45m")
+}
+
+// Set foreground color
 func (ansi Ansi) ColorCyanFG() {
 	fmt.Fprintf(os.Stdout, "\033[36m")
 }
 
+// Set background color
 func (ansi Ansi) ColorCyanBG() {
 	fmt.Fprintf(os.Stdout, "\033[46m")
 }
 
-// Verbose takes a string and formats and outputs to STDERR
+// Set foreground color
+func (ansi Ansi) ColorWhiteFG() {
+	fmt.Fprintf(os.Stdout, "\033[37m")
+}
+
+// Set background color
+func (ansi Ansi) ColorWhiteBG() {
+	fmt.Fprintf(os.Stdout, "\033[47m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorDefaultFG() {
+	fmt.Fprintf(os.Stdout, "\033[39m")
+}
+
+// Set background color
+func (ansi Ansi) ColorDefaultBG() {
+	fmt.Fprintf(os.Stdout, "\033[49m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorBrightBlackFG() {
+	fmt.Fprintf(os.Stdout, "\033[90m")
+}
+
+// Set background color
+func (ansi Ansi) ColorBrightBlackBG() {
+	fmt.Fprintf(os.Stdout, "\033[100m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorBrightRedFG() {
+	fmt.Fprintf(os.Stdout, "\033[91m")
+}
+
+// Set background color
+func (ansi Ansi) ColorBrightRedBG() {
+	fmt.Fprintf(os.Stdout, "\033[101m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorBrightGreenFG() {
+	fmt.Fprintf(os.Stdout, "\033[92m")
+}
+
+// Set background color
+func (anis Ansi) ColorBrightGreenBG() {
+	fmt.Fprintf(os.Stdout, "\033[102m")
+}
+
+// Set foreground color
+func (anis Ansi) ColorBrightYellowFG() {
+	fmt.Fprintf(os.Stdout, "\033[93m")
+}
+
+// Set background color
+func (ansi Ansi) ColorBrightYellowBG() {
+	fmt.Fprintf(os.Stdout, "\033[103m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorBrightBlueFG() {
+	fmt.Fprintf(os.Stdout, "\033[94m")
+}
+
+// Set background color
+func (ansi Ansi) ColorBrightBlueBG() {
+	fmt.Fprintf(os.Stdout, "\033[104m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorBrightMagentaFG() {
+	fmt.Fprintf(os.Stdout, "\033[95m")
+}
+
+// Set background color
+func (ansi Ansi) ColorBrightMagentaBG() {
+	fmt.Fprintf(os.Stdout, "\033[105m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorBrightCyanFG() {
+	fmt.Fprintf(os.Stdout, "\033[96m")
+}
+
+// Set background color
+func (ansi Ansi) ColorBrightCyanBG() {
+	fmt.Fprintf(os.Stdout, "\033[106m")
+}
+
+// Set foreground color
+func (ansi Ansi) ColorBrightWhiteFG() {
+	fmt.Fprintf(os.Stdout, "\033[97m")
+}
+
+// Set background color
+func (ansi Ansi) ColorBrightWhiteBG() {
+	fmt.Fprintf(os.Stdout, "\033[107m")
+}
+
+// Set foreground 256-bit color of value
+func (ansi Ansi) Color256FG(value byte) {
+	fmt.Fprintf(os.Stdout, "\033[38;5;%dm", value)
+}
+
+// Set background 256-bit color of value
+func (ansi Ansi) Color256BG(value byte) {
+	fmt.Fprintf(os.Stdout, "\033[38;5;%dm", value)
+}
+
+// Set foreground RGB color
+func (ansi Ansi) ColorRgbFG(red, green, blue byte) {
+	fmt.Fprintf(os.Stdout, "\033[38;2;%d;%d;%dm", red, green, blue)
+}
+
+// Set background RGB color
+func (ansi Ansi) ColorRgbBG(red, green, blue byte) {
+	fmt.Fprintf(os.Stdout, "\033[48;2;%d;%d;%dm", red, green, blue)
+}
+
+/*
+func (ansi Ansi) ScreenMode40x25Monochrome() {
+	fmt.Fprintf(os.Stdout, "\033[=0h")
+}
+
+func (ansi Ansi) ScreenMode40x25Color() {
+	fmt.Fprintf(os.Stdout, "\033[=1h")
+}
+
+func (ansi Ansi) ScreenMode80x25Monochrome() {
+	fmt.Fprintf(os.Stdout, "\033[=2h")
+}
+
+func (ansi Ansi) ScreenMode80x25Color() {
+	fmt.Fprintf(os.Stdout, "\033[=3h")
+}
+
+func (ansi Ansi) ScreenMode320x200Color() {
+	fmt.Fprintf(os.Stdout, "\033[=4h")
+}
+
+func (ansi Ansi) ScreenMode320x200Monochrome() {
+	fmt.Fprintf(os.Stdout, "\033[=5h")
+}
+
+func (ansi Ansi) ScreenMode640x200
+*/
+
+// Takes a string and formats and outputs to STDERR
 func Verbose(text string, enabled bool) {
 	now := time.Now()
 	if enabled {
@@ -478,7 +698,7 @@ func Verbose(text string, enabled bool) {
 	}
 }
 
-// Er checks for error state, if found, prints the error to STDERR and exits
+// Checks for error state, if found, prints the error to STDERR and exits
 func Er(err error) {
 	now := time.Now()
 	pc, file, line, ok := runtime.Caller(1)
@@ -495,7 +715,7 @@ func Er(err error) {
 	}
 }
 
-// HumanizeBytes retruns a string of the human readabel form.
+// Retruns a string of the human readabel form.
 // i.e. 1024 retruns "1 KiB" or "1.03 KB" for SI Units
 func HumanizeBytes(number int64, SIUnits bool) string {
 	if SIUnits {
@@ -535,7 +755,7 @@ func HumanizeBytes(number int64, SIUnits bool) string {
 	}
 }
 
-// PrintValue returns a string to display the value(s) and datatype(s)
+// Returns a string to display the value(s) and datatype(s)
 func PrintValue(input any) string {
 	var output string
 	t := reflect.ValueOf(input).Kind()
