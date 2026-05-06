1ce74740771a6c7f8008262a2d6c624a98077fd7
epyon0 <epyon0@proton.me>
2026-05-06 07:55:57 -0500

    create PrintBytes function

--- libEpyon.go
+++ libEpyon.go
@@ -200,6 +200,13 @@ func AsciiChar(input byte) string {
 func PrintBytes(input []byte) string {
 	var output string
 
+	for i, item := range input {
+		output += fmt.Sprintf("%08b 0x%02X [%3.3s]   ", item, item, AsciiChar(item))
+		if (i+1)%8 == 0 {
+			output += "\n"
+		}
+	}
+
 	return output
 }
 
